22b1ba30a714023e2602689109ee4b1ff7a48f59
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-07-14 18:04:22 +0530

    added heading and paragraph

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div>page</div>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
+            <h1 className="text-4xl font-bold mb-4">Welcome to Our E-commerce Website</h1>
+            <p className="text-lg text-gray-700">This is the homepage.</p>
+        </div>
     )
 }
 
