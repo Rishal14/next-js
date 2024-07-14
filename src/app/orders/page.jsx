2161ab9743b80dbfdeca2ef68f4d0e73eb618311
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-07-14 19:31:55 +0530

    made order page with details

---
+++ src/app/orders/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Orders() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-pink-100">
+            <h1 className="text-4xl font-bold mb-4">Orders</h1>
+            <p className="text-lg text-gray-700">Your order history.</p>
+        </div>
+    );
+}
