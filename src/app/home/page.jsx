bff2a6678e986034ad94f09ecb5bd7a453e1daf0
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-07-14 18:07:28 +0530

    changed text color

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
-            <h1 className="text-4xl font-bold mb-4">Welcome to Our E-commerce Website</h1>
+            <h1 className="text-4xl font-bold mb-4 text-purple-500">Welcome to Our E-commerce Website</h1>
             <p className="text-lg text-gray-700">This is the homepage.</p>
         </div>
     )
